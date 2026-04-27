b8bb6857423ac52f3301e12a39daf85b7dceeb79
mysterayoub <ayoubie65@outlook.com>
2026-04-27 19:20:13 +0200

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -180,7 +180,6 @@ export async function createInitialHelpMenu(client) {
     );
 
     const buttonRow = new ActionRowBuilder().addComponents([
-        bugReportButton,
         supportButton,
         touchpointButton,
     ]);
